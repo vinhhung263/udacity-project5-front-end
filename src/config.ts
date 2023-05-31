5e0e6e22f4cc95b2a1eefb3e5a54309648bcbb91
vinhhung263 <hungpv263@gmail.com>
2023-05-31 10:57:19 +0700

    first build

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '0bqa8uaai2'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-gar55nt1nhag3aeg.us.auth0.com',            // Auth0 domain
+  clientId: 'UE6OT98w7SFuCHXWfXBKafnN8ainXsMF',
+  callbackUrl: 'http://udacity-hungpv-bucket-proj5.s3-website-us-east-1.amazonaws.com/callback'
+}
